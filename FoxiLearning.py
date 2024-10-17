9a1a7f7ed87200111dcf668657490c17d8e94ce2
ChinSiang Ang <37194364-angchinsiang38@users.noreply.replit.com>
2024-10-17 01:44:27 +0000

    Initial commit

---
+++ FoxiLearning.py
@@ -0,0 +1,171 @@
+#!pip install youtube-transcript-api
+#!pip install openai
+
+import streamlit as st
+import os
+from openai import OpenAI
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from youtube_transcript_api import YouTubeTranscriptApi
+import webbrowser
+import requests
+#only to wrap the colab output
+from IPython.display import HTML, display
+import random
+
+# def set_css():
+#     display(HTML('''
+#     <style>
+#       pre {
+#           white-space: pre-wrap;
+#       }
+#     </style>
+#     '''))
+# get_ipython().events.register('pre_run_cell', set_css)
+#inputTranscript = YouTubeTranscriptApi.get_transcript('1FZ0A1QCMWc')
+client = OpenAI(api_key=st.secrets['OpenAI_API_KEY'])
+
+# Set up the title and instructions
+st.title("Content Search App")
+
+# Ask the user to input their search query
+user_input = st.text_input("Enter the topic you are interested in:")
+inputTranscript = ''
+result = ''
+
+pages = {
+    "Page 1":
+    "Welcome to Page 1! This is the first page of our multi-page application.",
+    "Page 2": "This is Page 2! Here we have some different content.",
+    "Page 3":
+    "You've reached Page 3! More interesting information can be found here.",
+    "Page 4": "This is the last page, Page 4! Thank you for browsing!"
+}
+
+if 'page_index' not in st.session_state:
+    st.session_state.page_index = 0
+
+
+# Function to go to the next page
+def next_page():
+    if st.session_state.page_index < len(pages) - 1:
+        st.session_state.page_index += 1
+
+
+# Function to go to the previous page
+def previous_page():
+    if st.session_state.page_index > 0:
+        st.session_state.page_index -= 1
+
+
+def TimeLine(inputTranscript):
+    response = client.chat.completions.create(
+        model='gpt-4o',
+        messages=[{
+            'role':
+            'system',
+            'content':
+            """You will help the user to check whether the input is a subset of user input . Please mention at which timeline in the video in the form of hours:minutes:seconds it is related and how it is related in the format of line by line."""
+        }, {
+            'role': 'user',
+            'content': f'{user_input}'
+        }],
+        max_tokens=2048,
+    )
+
+
+def Summarization(inputTranscript):
+    response = client.chat.completions.create(
+        model='gpt-4o',
+        messages=[{
+            'role':
+            'system',
+            'content':
+            """You are a good story point retriever. You concludes all the important points that are from different timeline that is mentioned in the input. Please come out with as detailed as possible conclusion to help user understand the content key points."""
+        }, {
+            'role': 'user',
+            'content': f'{user_input}'
+        }],
+        max_tokens=2048,
+    )
+
+
+# Define Youtube API Key and search query
+
+API_KEY = 'AIzaSyBmc8Xkl-EBWj8BJJAzA_kJS5B9r2tTmgI'  # Replace with your YouTube Data API key
+
+
+def Youtube_search(user_input):
+
+    # YouTube API endpoint and parameters
+    api_url = 'https://www.googleapis.com/youtube/v3/search'
+    params = {
+        'part': 'snippet',
+        'q': user_input,
+        'type': 'video',
+        'key': API_KEY,
+        'maxResults': 100  # Number of videos to retrieve
+    }
+
+    # Make a request to YouTube API
+    response = requests.get(api_url, params=params)
+    #print(response)
+    # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+
+        # Create an HTML file to display the videos
+        with open('youtube_videos.html', 'w') as f:
+            f.write(
+                '<html><head><style>body{color:white}</style></head><body>\n')
+            f.write(f'<h1>Search results for "{user_input}"</h1>\n')
+
+            videos = data['items']
+            random.shuffle(videos)
+            # Select the top 'num_to_select' videos
+            selected_videos = videos[:5]
+
+            # Loop through each video in the response and embed it
+            for item in selected_videos:
+                video_id = item['id']['videoId']
+                title = item['snippet']['title']
+                description = item['snippet']['description']
+
+                # Embed the video using an iframe
+                f.write(f'<h3>{title}</h3>\n')
+                f.write(
+                    f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" frameborder="0" allowfullscreen></iframe>\n'
+                )
+                f.write(f'<p>{description}</p>\n')
+                f.write('<hr>\n')
+                f.write('</body></html>\n')
+
+
+# Navigation buttons
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Back"):
+        previous_page()
+
+with col2:
+    if st.button("Next"):
+        next_page()
+
+if st.button("Submit"):
+    Youtube_search(user_input)
+    # Path to the HTML file
+    html_file_path = os.path.abspath('youtube_videos.html')
+    # Check if the HTML file exists
+    if os.path.exists(html_file_path):
+        # Read the HTML file content
+        with open(html_file_path, 'r') as f:
+            html_content = f.read()
+
+        # Display the HTML content in Streamlit
+        st.components.v1.html(html_content, height=600, scrolling=True)
+
+    else:
+        st.error("HTML file not found.")
---
+++ StoryBook.py
@@ -0,0 +1,79 @@
+import os
+from openai import OpenAI
+#from google.colab import userdata
+#rom IPython.display import Image
+import streamlit as st
+
+client = OpenAI(api_key=st.secrets['OpenAI_API_KEY'])
+
+
+def story_gen(prompt):
+    system_prompt = """
+  You are a world renowned author for young adults fiction short stories.
+  Given a concept, generate a short story relevant to the themes of the concept with a twist ending.
+  Total length of story is kept in 100 words.
+  """
+
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=[{
+            'role': 'system',
+            'content': system_prompt
+        }, {
+            'role': 'user',
+            'content': prompt
+        }],
+        temperature=1.2,
+        max_tokens=1000,
+    )
+
+    return response.choices[0].message.content
+
+
+def art_gen(prompt):
+    response = client.images.generate(
+        model='dall-e-3',
+        prompt=prompt,
+        size='1024x1024',
+        n=1,
+    )
+
+    return response.data[0].url
+
+
+    #Cover prompt design
+def design_gen(prompt):
+    system_prompt = """
+      You will be given a short story. Generate a prompt for a cover art that is suitable for the story. The prompt is for dall-e-3.
+
+      """
+    response = client.chat.completions.create(model='gpt-4o-mini', messages=[{
+     "role":
+                             "system",
+                      "content":
+                                                  system_prompt
+                                              }, {
+                                                  "role": "user",
+                                                  "content": prompt
+                                              }],
+                                              temperature=1.2,
+                                              max_tokens=2000)
+    return response.choices[0].message.content
+
+
+prompt = st.text_input("Enter a prompt")
+if st.button("Generate"):
+    story = story_gen(prompt)
+    design = design_gen(story)
+    art = art_gen(design)
+    original = art_gen(story)
+
+    st.caption(design)    
+    st.divider()
+    st.write(story)
+    st.divider()
+    st.image(art)
+    st.divider()
+    st.image(original)
+    
+    
